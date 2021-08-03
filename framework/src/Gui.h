63b7563d9aa4ff2e60c43b1ecdb12dd75ced30d6
Ryan McCrystal <rrmccrystal@gmail.com>
2021-08-02 23:35:47 -1000

    Removed files

--- framework/src/Gui.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by draven on 7/6/2021.
-//
-
-
-#pragma once
-
-extern "C" void show_memory_editor();
-
--- framework/src/Offsets.h
+++ framework/src/Offsets.h
@@ -2,7 +2,7 @@ namespace offsets
 {
     namespace directx
     {
-        constexpr auto command_queue = 0x189B3D28;
-        constexpr auto swap_chain = 0x189B90A0;
+        constexpr auto command_queue = 0x18F76D28;
+        constexpr auto swap_chain = 0x18F7C0A0;
     }
 }
